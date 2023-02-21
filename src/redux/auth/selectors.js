b76035fcdb8694b5fc8044651f9eaa91dd817fc2
SunAndrea <andriishtohryn@gmail.com>
2023-02-21 17:09:51 +0200

    day endpoints

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -3,3 +3,5 @@ export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 export const selectUser = state => state.auth.user;
 
 export const selectSid = state => state.auth.sid;
+
+export const selectRefreshToken = state => state.auth.refreshToken;
