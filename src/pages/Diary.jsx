0a17f922321e29988792b5fccc58092487972f9d
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-02-23 22:43:06 +0200

    add Sumery

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -2,6 +2,7 @@ import AddProduct from 'components/AddProduct/AddProduct';
 // import { InformationBlock } from 'components/InformationBlock/InformationBlock';
 import ProductList from 'components/ProductList/ProductList';
 import { Calendar } from 'components/Calendar/Calendar';
+import { Summary } from 'components/Summary/Summary';
 
 export const DiaryPage = () => {
   return (
@@ -9,6 +10,7 @@ export const DiaryPage = () => {
       <Calendar></Calendar>
       <AddProduct />
       <ProductList />
+      <Summary />
       {/* <InformationBlock /> */}
     </>
   );
