c3f98afebfca35a7a8951c4dd97d4578490f2760
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-02-24 13:43:43 +0200

    del errors

--- src/components/FoodList/FoodList.jsx
+++ src/components/FoodList/FoodList.jsx
@@ -1,14 +1,12 @@
-import { memo, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchDaily, fetchDailyRateByUserId } from 'redux/daily-rate/operation';
+import { memo } from 'react';
+import { useSelector } from 'react-redux';
 import { notAllowedProducts } from 'redux/daily-rate/selection';
 import { randomProducts } from 'redux/daily-rate/selection';
 import { selectAccessProducts } from 'redux/auth/selectors';
 
 import { useAuth } from 'hooks/useAuth';
 
-export const FoodList = memo(({ values }) => {
-  const dispatch = useDispatch();
+export const FoodList = memo(() => {
   const { user, isLoggedIn } = useAuth();
   const randomProductsState = useSelector(randomProducts);
   const randomProductsAuthState = useSelector(selectAccessProducts);
@@ -17,17 +15,6 @@ export const FoodList = memo(({ values }) => {
     ? randomProductsAuthState
     : randomProductsState;
 
-  const userLoginedInfo = {
-    userId: user.id,
-    userData: values,
-  };
-
-  useEffect(() => {
-    isLoggedIn
-      ? dispatch(fetchDailyRateByUserId(userLoginedInfo))
-      : dispatch(fetchDaily(values));
-  }, [values]);
-
   let notAllowedProductsState = useSelector(notAllowedProducts);
   if (isLoggedIn) {
     notAllowedProductsState = user.userData.notAllowedProducts;
@@ -35,8 +22,8 @@ export const FoodList = memo(({ values }) => {
 
   return (
     <ol>
-      {notAllowedProductsList.map(item => {
-        return <li>{item}</li>;
+      {notAllowedProductsList.map((item, index) => {
+        return <li key={index}>{item}</li>;
       })}
     </ol>
   );
